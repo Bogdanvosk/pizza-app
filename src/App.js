236d094248960719cab83595e7fe9fcfb940cf90
Bogdan Voskanov <bogdanvoskanov@gmail.com>
2021-04-27 22:56:56 +0300

    Filtration and sort added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React from 'react'
 import { Route } from 'react-router'
 
 import { Header } from './components'
 import { Home, Cart } from './pages'
-import { setPizzas } from './redux/actions/pizzas'
+
 
 function App() {
-    const dispatch = useDispatch()
-    const items = useSelector(state => state.pizzas.items)
-
-    useEffect(() => {
-        axios.get('http://localhost:3000/db.json').then(({ data }) => {
-            dispatch(setPizzas(data.pizzas))
-        })
-    }, [])
 
     return (
         <div className='wrapper'>
             <Header />
             <div className='content'>
-                <Route exact path='/' render={() => <Home pizzas={items} />} />
+                <Route exact path='/' component={Home} />
                 <Route path='/cart' component={Cart} />
             </div>
         </div>
